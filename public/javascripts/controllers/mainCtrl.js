e36ad6dcef1f4c0f666a7d043d5d9ffc86cc8d69
ArthurKishinets <arthurkishinets@yahoo.com.ua>
2016-09-17 22:33:08 +0300

    removed protocol

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -1,6 +1,6 @@
 module.exports = function($http, $scope) {
     var main = this;
-    $http.get("http://content.guardianapis.com/search?order-by=newest&q=literature%20books&show-blocks=all&api-key=test")
+    $http.get("//content.guardianapis.com/search?order-by=newest&q=literature%20books&show-blocks=all&api-key=test")
         .then(function(response) {
             main.articles = response.data.response.results.slice(5);
             console.log(main.articles);
